e64fd484de26aed0ca0a9b188f572b685136e8a6
sebasmore1406 <sebasmore1406@gmail.com>
2023-10-24 09:30:38 -0600

    Created struture for program

--- conversor_pc.py
+++ conversor_pc.py
@@ -1,1 +1,14 @@
+from tkinter import Tk, StringVar, messagebox
+from tkinter.ttk import Label, Frame, Button, Entry, Combobox, Style
 
+###### FUNCIONES ######
+###### FUNCIONES ######
+
+####### ESTILOS #######
+harrys = Style()
+####### ESTILOS #######
+
+## VENTANA PRINCIPAL ##
+root = Tk()
+
+root.mainloop()
